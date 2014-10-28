1ffd235f4943c8b02f74b902f4aa419b85a5130e
YANKE Guo <me@yanke.io>
2014-10-28 23:09:32 +0800

    update

--- files/ACKeyboardObserver/ACKeyboardObserver.m
+++ files/ACKeyboardObserver/ACKeyboardObserver.m
@@ -1,108 +1,9 @@
----
-layout: post
-title: Introduction to ACKeyboardObserver
-categories: ios
----
-
-A small class which turns 6 keyboard notifications into a delegate with two methods, extracting values from `NSNotification`,  and providing an easy-to-use function for making a keyboard related animation.
-
-## Usage
-
-- Implement `ACKeyboardObserverDelegate` in view controller.
-
-```objective-c
-- (void)keyboardWillEmitEvent:(ACKeyboardEvent)event withChange:(ACKeyboardChange)change;
-- (void)keyboardDidEmitEvent:(ACKeyboardEvent)event withChange:(ACKeyboardChange)change;
-```
-
-- Initialize a instance.
-
-```objective-c
-self.keyboardObserver = [ACKeyboardObserver observerWithDelegate:self];
-[self.keyboardObserver start];
-```
-
-- Use `ACKeyboardFastAnimate` to create a keyboard-related animation in delegate methods.
-
-## Files
-
-- [ACKeyboardObserver.h](/files/ACKeyboardObserver/ACKeyboardObserver.h)
-
-- [ACKeyboardObserver.m](/files/ACKeyboardObserver/ACKeyboardObserver.m)
-
-### ACKeyboardObserver.h
-
-```objective-c
-//
-//  ACKeyboardObserver.h
-//
-//  Created by YANKE Guo on 12/12/13.
-//
-
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-
-#pragma mark - Basic Struct
-
-typedef struct {
-  CGRect                  beginRect;
-  CGRect                  endRect;
-  NSTimeInterval          duration;
-  UIViewAnimationCurve    animationCurve;
-} ACKeyboardChange;
-
-typedef NS_ENUM(NSInteger, ACKeyboardEvent) {
-  ACKeyboardEventShow,
-  ACKeyboardEventHide,
-  ACKeyboardEventChangeFrame
-};
-
-#pragma mark - Fast keyboard animation
-
-typedef void (^ACKeyboardAnimationBlock)(CGRect beginRect,CGRect endRect);
-
-//  convert UIViewAnimationCurve to UIViewAnimationOptions
-UIViewAnimationOptions IGAnimationOptionsWithCurve(UIViewAnimationCurve curve);
-
-//  convient method to make a animation attached with a ACKeyboardChange
-void ACKeyboardFastAnimate(ACKeyboardChange change,ACKeyboardAnimationBlock block);
-
-#pragma mark - Delegate Protocol
-
-@class ACKeyboardObserver;
-
-@protocol ACKeyboardObserverDelegate <NSObject>
-
-@optional
-
-- (void)keyboardWillEmitEvent:(ACKeyboardEvent)event withChange:(ACKeyboardChange)change;
-- (void)keyboardDidEmitEvent:(ACKeyboardEvent)event withChange:(ACKeyboardChange)change;
-
-@end
-
-@interface ACKeyboardObserver : NSObject
-
-@property (nonatomic,assign,readonly) id<ACKeyboardObserverDelegate> delegate;
-@property (nonatomic,assign,readonly,getter = isObserving) BOOL observing;
-
-+ (instancetype)observerWithDelegate:(id<ACKeyboardObserverDelegate>)delegate;
-
-- (id)initWithDelegate:(id<ACKeyboardObserverDelegate>)delegate;
-
-- (void)start;
-
-- (void)stop;
-
-@end
-```
-
-### ACKeyboardObserver.m
-
-```objective-c
 //
 //  ACKeyboardObserver.m
+//  IGKitDemo
 //
 //  Created by YANKE Guo on 12/12/13.
+//  Copyright (c) 2013 IREUL Guo. All rights reserved.
 //
 
 #import "ACKeyboardObserver.h"
@@ -228,4 +129,3 @@ ACKeyboardChange ACKeyboardChangeFromDictionary(NSDictionary * dict)
 }
 
 @end
-```
